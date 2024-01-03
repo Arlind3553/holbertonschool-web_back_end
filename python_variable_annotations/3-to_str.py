6715da38d25ca63704609f1558fae74350deec2a
arlind3553 <georgetesco@yahoo.com>
2024-01-03 16:16:54 +0000

    task 1

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Simple module for strongly
+typed python
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+     function to_str that takes a float n as argument
+     and returns the string representation of the float.
+    '''
+    return str(n)
