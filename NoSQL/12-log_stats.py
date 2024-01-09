a8fe84767ccf04ccb038ae4f606152d5ac3f60b8
arlind3553 <georgetesco@yahoo.com>
2024-01-09 16:27:52 +0100

    task 1

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     logs_collection = client.logs.nginx
 
-    print(f"{logs_collection.count_documents({})} logs")
-    print(f"\tmethod GET: {logs_collection.count_documents({'method': 'GET'})}")
-    print(f"\tmethod POST: {logs_collection.count_documents({'method': 'POST'})}")
-    print(f"\tmethod PUT: {logs_collection.count_documents({'method': 'PUT'})}")
-    print(f"\tmethod PATCH: {logs_collection.count_documents({'method': 'PATCH'})}")
-    print(f"\tmethod DELETE: {logs_collection.count_documents({'method': 'DELETE'})}")
-    print(f"{logs_collection.count_documents({'path': '/status'})} status check")
+    print(f"{logs_collection.count({})} logs")
+    print(f"\tmethod GET: {logs_collection.count({'method': 'GET'})}")
+    print(f"\tmethod POST: {logs_collection.count({'method': 'POST'})}")
+    print(f"\tmethod PUT: {logs_collection.count({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {logs_collection.count({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {logs_collection.count({'method': 'DELETE'})}")
+    print(f"{logs_collection.count({'path': '/status'})} status check")
