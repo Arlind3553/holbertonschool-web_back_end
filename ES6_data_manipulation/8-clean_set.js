38cf14c5f005870eb64c2533eb1bb7444a632b32
arlind3553 <georgetesco@yahoo.com>
2024-02-01 11:02:23 +0000

    task 1

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
-/*eslint-disable*/
 export default function cleanSet(set, startString) {
+    if (typeof startString !== 'string') {
+    return '';
+    }
     const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
     const resultString = filteredValues.map(value => value.slice(startString.length)).join('-');
     if (startString == '') {
