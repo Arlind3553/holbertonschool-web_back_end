795e643df3e0b697889108e699962bb80f77d0a4
Arlind3553 <135588828+Arlind3553@users.noreply.github.com>
2024-03-05 13:29:17 +0100

    Update 0-get_list_students.js

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,9 @@
 /*eslint-disable*/
-export default function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-  ];
+export default function getListStudents(id, firstName, location) { 
+    const students = [
+    {id: 1, firstName: "Guillaume", location: "San Francisco"},
+    {id: 2, firstName: "James",location: "Columbia"},
+    {id: 5, firstName: "Serena",location: "San Francisco"}
+    ];
+    return students;
 }
